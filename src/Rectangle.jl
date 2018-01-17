7ea5edaf9705595c9731bdc57cfda4f33ec8c536
Sambit Kumar Dash <sambitdash@gmail.com>
2018-01-18 03:59:04 +0530

    2-D Rectangle with CSG operations.

--- src/Rectangle.jl
+++ src/Rectangle.jl
@@ -1,5 +1,59 @@
+__precompile__()
+
 module Rectangle
 
-# package code goes here
+export  Rect,
+            union,
+            intersect,
+            inside
+
+import Base: ==, union, intersect
+
+struct Rect{T <: Number}
+    lx::T
+    ly::T
+    rx::T
+    ry::T
+    function Rect{T}(lx::T, ly::T, rx::T, ry::T) where {T <: Number}
+        @assert lx <= rx && ly <= ry "Invalid values."
+        return new(lx, ly, rx, ry)
+    end
+end
+
+Rect(lx::T, ly::T, rx::T, ry::T) where {T <: Number} = Rect{T}(lx, ly, rx, ry)
+
+function union(r1::Rect, r2::Rect)
+    lx = min(r1.lx, r2.lx)
+    ly = min(r1.ly, r2.ly)
+    rx = max(r1.rx, r2.rx)
+    ry = max(r1.ry, r2.ry)
+    return Rect(lx, ly, rx, ry)
+end
+
+function intersect(r1::Rect{T}, r2::Rect{T}) where T <: Number
+    lx = max(r1.lx, r2.lx)
+    ly = max(r1.ly, r2.ly)
+    rx = min(r1.rx, r2.rx)
+    ry = min(r1.ry, r2.ry)
+    (lx > rx || ly > ry) && return nothing
+    return Rect(lx, ly, rx, ry)
+end
+
+pcTol(::Type{T}) where {T <: Integer} = zero(T)
+pcTol(::Type{T}) where {T <: Rational} = zero(T)
+pcTol(::Type{T}) where {T <: Number}  = T(0.000001)
+
+function ==(r1::Rect{T}, r2::Rect{T}) where {T <: Number}
+    dlx = abs(r1.lx - r2.lx)
+    dly = abs(r1.ly - r2.ly)
+    drx = abs(r1.rx - r2.rx)
+    dry = abs(r1.ry - r2.ry)
+    return dlx <= pcTol(T) && dly <= pcTol(T) && drx <= pcTol(T) && dry <= pcTol(T)
+end
+
+inside(p::Tuple{T, T}, r::Rect{T}) where T <: Number =
+    r.lx <= p[1] <= r.rx && r.ly <= p[2] <= r.ry
+
+inside(ri::Rect, ro::Rect) = intersect(ri, ro) == ri
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using Rectangle
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "Rectangle" begin
+    @test Rect(0, 0, 10, 10) == Rect(0, 0, 10, 10)
+    @test union(Rect(0, 0, 10, 5), Rect(11, -1, 12, 13)) == Rect(0, -1, 12, 13)
+    @test intersect(Rect(0, 0, 10, 5), Rect(11, -1, 12, 13)) == nothing
+    @test inside((1,2), Rect(0, 0, 10, 10))
+    @test inside(Rect(1, 1, 3, 3), Rect(0, 0, 10, 10))
+    @test Rect(0.0, 0.0, 10.0, 10.0) == Rect(0.0000001, 0.0000001, 10.0, 10.0)
+    @test Rect(0.0, 0.0, 10.0, 10.0) != Rect(0.000002, 0.0000001, 10.0, 10.0)
+end
